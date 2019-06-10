4a8d8eae09d6e69cef90e0f8bf3ed26b4b4a3a3e
silverburd <johnburd19@gmail.com>
2019-06-09 20:29:52 -0500

    added segment module

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const sleep = require('sleep');
-const Gpio = require('onoff').Gpio;
-const Motor = require('./modules/motor.js');
+
+const Motor         = require('./modules/motor.js');
 const ShiftRegister = require('./modules/shiftRegister.js'); 
+const Segment       = require('./modules/segment.js'); 
 
 const data  = 17;
 const clock = 27;
@@ -17,57 +18,9 @@ const m4 = new Motor(4);
 const m5 = new Motor(5);
 const m6 = new Motor(6);
 
-const zero  = [0, 1, 2, 3, 4, 5];
-const one   = [1, 2];
-const two   = [0, 1, 3, 4, 6];
-const three = [0, 1, 2, 3, 6];
-const four  = [1, 2, 3, 5, 6];
-const five  = [0, 2, 3, 6, 6];
-const six   = [0, 2, 3, 4, 5, 6];
-const seven = [0, 1, 2];
-const eight = [0, 1, 2, 3, 4, 5, 6];
-const nine  = [0, 1, 2, 5, 6];
-
-const testOn  = eight;
-const testOff  = [];
-
 const motors = [ m0, m1, m2, m3, m4, m5, m6 ];
 
-function getOnOff(motors, number) {
-    let on  = 0;
-    let off = 0;
+const s0 = new Segment(sr0, motors);
 
-    motors.forEach( motor => {
-        if (number.includes(motor._motor) && !motor._state) {
-            // console.log(motor);
-            on  = on + motor._dir + motor._step;
-            off = off + motor._dir;
-            motor._state = true;
-        } else if (!number.includes(motor._motor) && motor._state) {
-            // console.log(motor);
-            off = off + motor._step;
-            motor._state = false;
-        }
-    })
-
-    return { on: on, off: off };
-}
-
-function rotate(on, off) {
-    for (let i = 0; i < 515; i++) {
-        sr0.shiftOut(on);
-        sr0.latch();
-        sr0.shiftOut(off);
-        sr0.latch();
-        sleep.usleep(1200);
-    }
-    console.log(on);
-    console.log(off);
-}
-
-let nums = getOnOff(motors, testOn);
-rotate(nums.on, nums.off);
-
-sleep.sleep(1);
-nums = getOnOff(motors, testOff);
-rotate(nums.on, nums.off);
+s0.set(2);
+s0.set('off');
---
+++ modules/segment.js
@@ -0,0 +1,69 @@
+const sleep = require('sleep');
+
+class Segment {
+
+    _chars = {
+        '0': [0, 1, 2, 3, 4, 5],
+        '1': [1, 2],
+        '2': [0, 1, 3, 4, 6],
+        '3': [0, 1, 2, 3, 6],
+        '4': [1, 2, 3, 5, 6],
+        '5': [0, 2, 3, 6, 6],
+        '6': [0, 2, 3, 4, 5, 6],
+        '7': [0, 1, 2],
+        '8': [0, 1, 2, 3, 4, 5, 6],
+        '9': [0, 1, 2, 5, 6],
+
+        'off': [],
+        'on':  [0, 1, 2, 3, 4, 5, 6]
+    };
+
+    constructor(shiftRegister, motors, value=null) {
+        this._sr     = shiftRegister;
+        this._motors = motors;
+        this._value  = value;
+    }
+
+    getOnOff(char) {
+        let on  = 0;
+        let off = 0;
+
+        this._motors.forEach( motor => {
+            if (char.includes(motor._motor) && !motor._state) {
+                // console.log(motor);
+                on  = on + motor._dir + motor._step;
+                off = off + motor._dir;
+                motor._state = true;
+            } else if (!char.includes(motor._motor) && motor._state) {
+                // console.log(motor);
+                off = off + motor._step;
+                motor._state = false;
+            }
+        })
+        this._on  = on;
+        this._off = off;
+
+        return { on: on, off: off };
+    }
+
+    rotate() {
+        for (let i = 0; i < 515; i++) {
+            this._sr.shiftOut(this._on);
+            this._sr.latch();
+            this._sr.shiftOut(this._off);
+            this._sr.latch();
+            sleep.usleep(1200);
+        }
+        console.log(this._on);
+        console.log(this._off);
+    }
+
+    set(char) {
+        this.getOnOff(this._chars[char]);
+        this.rotate();
+        this._value = char;
+    }
+
+}
+
+module.exports = Segment;
