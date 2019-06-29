5914cbb006717f8238ac4b3974861bce493efa7e
silverburd <johnburd19@gmail.com>
2019-06-29 17:51:41 -0500

    renamed file

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const sleep = require('sleep');
+const express = require('express');
+const app = express();
 
-const Motor         = require('./modules/motor.js');
 const ShiftRegister = require('./modules/shiftRegister.js'); 
-const Segment       = require('./modules/segment.js'); 
+const Motor         = require('./modules/motor.js');
+const Digit         = require('./modules/digit.js'); 
 
 const data  = 17;
 const clock = 27;
@@ -20,7 +22,40 @@ const m6 = new Motor(6);
 
 const motors = [ m0, m1, m2, m3, m4, m5, m6 ];
 
-const s0 = new Segment(sr0, motors);
+const s0 = new Digit(sr0, motors);
 
-s0.set(2);
-s0.set('off');
+
+app.get('/char/:char', (req, res) => {
+    let char = req.params['char'];
+    s0.set(char);
+    res.send('hello world');
+});
+app.get('/play', (req, res) => {
+    //let char = req.params['char'];
+    //s0.set(char);
+    for (let x = 0; x < 10; x++ ) {
+        s0.set(x);
+    }
+    s0.set('off');
+    res.send('hello world');
+});
+function time() {
+    let today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    return m;
+}
+ 
+(setInterval(function() {
+    let minute = time();
+    if (minute < 10) {
+        minute = '0' + time().toString();
+        minute = minute[1];
+    } else {
+        minute = time().toString()[1];
+    }
+    console.log(minute);
+    s0.set(minute);
+}, 500));
+
+app.listen(3000);
